70fec921b2d3885dd7bde8e259def60304107b21
Cedrick <lacro.cedrick@outlook.com>
2025-02-23 20:00:20 -0700

    Update notes-routes

--- routes/notes-routes.js
+++ routes/notes-routes.js
@@ -1,8 +1,32 @@
 import express from 'express';
-import { getCategories } from '../controllers/notes-controller.js';
+import { getCategories, getNotesByCategory, getNoteDetails, createNote, getCommentsByNote, addCommentToNote, editComment, updatePathsForNote, updateNoteDetails, getAllNoteTitles } from '../controllers/notes-controller.js';
 
 const router = express.Router();
 
-router.get('/categories', getCategories);
+router.route('/')
+    .post(createNote);
+
+router.route('/titles')
+    .get(getAllNoteTitles);
+
+router.route('/categories')
+    .get(getCategories);
+
+router.route('/category/:categoryId')
+    .get(getNotesByCategory);
+
+router.route('/:noteId')
+    .get(getNoteDetails)
+    .put(updateNoteDetails);
+
+router.route('/:noteId/paths')
+    .put(updatePathsForNote);
+
+router.route('/:noteId/comments')
+    .get(getCommentsByNote)
+    .post(addCommentToNote);
+
+router.route('/:noteId/comments/:commentId')
+    .put(editComment);
 
 export default router;
