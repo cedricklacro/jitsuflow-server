2ad3fd0be11b9079391aca1893ef677da7e88b73
Cedrick <lacro.cedrick@outlook.com>
2025-02-23 19:57:22 -0700

    Update migrate files for notes and categories table

--- migrations/20030000000000_create_notes_table.js
+++ migrations/20030000000000_create_notes_table.js
@@ -2,7 +2,7 @@ export function up(knex) {
   return knex.schema.createTable('notes', (table) => {
     table.increments('note_id').primary();
     table.integer('user_id').unsigned().notNullable().references('user_id').inTable('users').onDelete('CASCADE');
-    table.integer('category_id').unsigned().notNullable().references('category_id').inTable('note_categories').onDelete('CASCADE');
+    table.integer('category_id').unsigned().nullable().references('category_id').inTable('note_categories').onDelete('CASCADE');
     table.string('title').notNullable();
     table.text('content');
     table.timestamp('created_at').defaultTo(knex.fn.now());
--- migrations/20060000000000_create_paths_table.js
+++ migrations/20060000000000_create_paths_table.js
@@ -2,9 +2,9 @@ export function up(knex) {
     return knex.schema.createTable('paths', (table) => {
         table.increments('path_id').primary();
         table.integer('user_id').unsigned().notNullable().references('user_id').inTable('users').onDelete('CASCADE');
-        table.integer('from_note').unsigned().references('note_id').inTable('notes').onDelete('SET NULL');
-        table.integer('to_note').unsigned().references('note_id').inTable('notes').onDelete('SET NULL');
-        table.enu('path_type', ['entry', 'exit', 'counter']).notNullable();
+        table.integer('from_note').unsigned().references('note_id').inTable('notes').onDelete('CASCADE');
+        table.integer('to_note').unsigned().references('note_id').inTable('notes').onDelete('CASCADE');
+        table.enu('path_type', ['entry_exit', 'counter']).notNullable();
     });
 }
 
