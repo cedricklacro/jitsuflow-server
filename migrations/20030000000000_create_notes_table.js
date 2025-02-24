b58d53e583b077e0199e75e707d507dd32f31c85
Cedrick <lacro.cedrick@outlook.com>
2025-02-24 00:14:03 -0700

    Update note table migrate file

--- migrations/20030000000000_create_notes_table.js
+++ migrations/20030000000000_create_notes_table.js
@@ -4,7 +4,7 @@ export function up(knex) {
     table.integer('user_id').unsigned().notNullable().references('user_id').inTable('users').onDelete('CASCADE');
     table.integer('category_id').unsigned().nullable().references('category_id').inTable('note_categories').onDelete('CASCADE');
     table.string('title').notNullable();
-    table.text('content');
+    table.text('content', 'longtext');
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
   });
